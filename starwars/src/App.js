52bb773518f328f218c04cbc426105630f5d92a7
kabeh2 <mattcnk@hotmail.com>
2019-07-29 23:15:15 -0400

    Mapped and Rendered First Page of Characters

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import axios from "axios";
+import Icon from "@material-ui/core/Icon";
+import { Typography } from "@material-ui/core";
+import MediaCard from "./components/card";
+
+import "./App.scss";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("https://swapi.co/api/people/");
+        setLoading(false);
+        // console.log(result.data.results);
+        setData(result.data.results);
+      } catch (error) {
+        console.log("there was an error");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // const a = data.map((data, index) => {
+  //   return <p key={index}>{data.name}</p>;
+  // });
+
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <CssBaseline />
+      <Typography variant="h1">React Wars</Typography>
+      <Icon>star</Icon>
+      <MediaCard data={data} />
+      {/* <MediaCard media={media1} initial={initial1} /> */}
     </div>
   );
-}
+};
 
 export default App;
